524bcb3b94ac258a2ea1398e0e04d4f50c812a0e
Marco Malatesta <60098727+marcomala46@users.noreply.github.com>
2024-07-24 11:10:10 +0200

    Create ASR_nb.sh

---
+++ ASR_nb.sh
@@ -0,0 +1,23 @@
+#!/bin/bash --login
+
+#!/bin/bash
+#SBATCH -p low-wn
+#SBATCH -c 20
+#SBATCH --mem 30G
+#SBATCH -t 4-00:00:00
+
+port=9393
+
+#Go to the folder you wanna run jupyter in
+cd $HOME
+
+#Pick a random or predefined port
+#port=$(shuf -i 6000-9999 -n 1)
+#Forward the picked port to the prince on the same port. Here log-x is set to be the prince login node.
+/usr/bin/ssh -N -f -R $port:localhost:$port login01
+
+
+#Start the notebook
+jupyter-lab --no-browser --ServerApp.allow_origin='https://colab.research.google.com' --port $port
+
+wait
